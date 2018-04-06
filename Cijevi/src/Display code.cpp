859cf6706d004ce2822214386e5a50ff69048a11
DevCodeZ1 <nkovacic01@gmail.com>
2018-04-06 15:54:10 +0200

    Dodani komentari za upute

--- Cijevi/src/Display code.cpp
+++ Cijevi/src/Display code.cpp
@@ -1,1 +1,7 @@
-#include <Arduino.h>
+/*Dodaj točan library https://www.youtube.com/watch?v=xVC0X_PE_XE
+Pogledaj Description
+Skini i dodaj library
+Skini scanner file i uploadaj
+koristi njegov id u kodu.
+Sve ti govori u videu.*/
+#include 
